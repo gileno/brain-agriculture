305b210ce582d7a518813ffaf18dcef7835433e7
gileno filho <gascf.cin@gmail.com>
2024-07-25 14:21:51 -0300

    data api added

--- brain_agriculture/farmers/views.py
+++ brain_agriculture/farmers/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from django.db.models import Sum
 
 from brain_agriculture.farmers.serializers import CitySerializer, CultureSerializer, FarmerSerializer
 from brain_agriculture.farmers.filtersets import CityFilterSet, CultureFilterSet, FarmerFilterSet
 from brain_agriculture.farmers.models import City, Culture, Farmer
+from brain_agriculture.farmers.constants import StateChoices
 
 
 class CityViewSet(viewsets.ReadOnlyModelViewSet):
@@ -28,3 +33,33 @@ class FarmerViewSet(viewsets.ModelViewSet):
     queryset = Farmer.objects.all()
     permission_classes = [IsAuthenticated]
     filterset_class = FarmerFilterSet
+
+    @action(detail=False, methods=['GET'])
+    def data(self, request):
+        total = Farmer.objects.count()
+        total_area = Farmer.objects.aggregate(total=Sum('total_area'))['total']
+        vegetation_area = Farmer.objects.aggregate(total=Sum('vegetation_area'))['total']
+        arable_area = Farmer.objects.aggregate(total=Sum('arable_area'))['total']
+        cultures = []
+        for culture in Culture.objects.all():
+            cultures.append({
+                'culture': culture.name,
+                'total': Farmer.objects.filter(cultures__in=[culture]).count(),
+            })
+        states = []
+        for state in StateChoices:
+            states.append({
+                'state': state,
+                'total': Farmer.objects.filter(city__state=state).count()
+            })
+        
+        return Response(
+            data={
+                'total': total,
+                'total_area': total_area,
+                'vegetation_area': vegetation_area,
+                'arable_area': arable_area,
+                'cultures': cultures,
+                'states': states,
+            }
+        )
